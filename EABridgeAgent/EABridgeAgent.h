53ea4e5e2329660b7c1dda15ecc35d1e0179e87c
gfwangfei <gfwangfei@oa.gf.com.cn>
2015-10-12 18:38:45 +0800

    Initial Commit

---
+++ EABridgeAgent/EABridgeAgent.h
@@ -0,0 +1,13 @@
+//
+//  EABridgeAgent.h
+//  EABridgeAgent
+//
+//  Created by rcio on 15/10/12.
+//  Copyright (c) 2015年 rcio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EABridgeAgent : NSObject
+
+@end
---
+++ EABridgeAgent/EABridgeAgent.m
@@ -0,0 +1,13 @@
+//
+//  EABridgeAgent.m
+//  EABridgeAgent
+//
+//  Created by rcio on 15/10/12.
+//  Copyright (c) 2015年 rcio. All rights reserved.
+//
+
+#import "EABridgeAgent.h"
+
+@implementation EABridgeAgent
+
+@end
